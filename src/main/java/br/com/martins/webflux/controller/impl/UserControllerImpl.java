c53435244c99df92640b531bdce771120a0505a0
AderisMartins <aderismartins.f@gmaiil.com>
2025-02-23 17:30:33 -0300

    implementação do findById e tratativas de retorno da consulta

--- src/main/java/br/com/martins/webflux/controller/impl/UserControllerImpl.java
+++ src/main/java/br/com/martins/webflux/controller/impl/UserControllerImpl.java
@@ -1,6 +1,7 @@
 package br.com.martins.webflux.controller.impl;
 
 import br.com.martins.webflux.controller.UserController;
+import br.com.martins.webflux.mapper.UserMapper;
 import br.com.martins.webflux.model.request.UserRequest;
 import br.com.martins.webflux.model.response.UserResponse;
 import br.com.martins.webflux.service.UserService;
@@ -18,6 +19,7 @@ import reactor.core.publisher.Mono;
 public class UserControllerImpl implements UserController {
 
     private final UserService service;
+    private final UserMapper mapper;
 
     @Override
     public ResponseEntity<Mono<Void>> save(UserRequest request) {
@@ -26,8 +28,8 @@ public class UserControllerImpl implements UserController {
     }
 
     @Override
-    public ResponseEntity<Mono<UserResponse>> find(String id) {
-        return null;
+    public ResponseEntity<Mono<UserResponse>> findById(String id) {
+        return ResponseEntity.ok().body(service.findById(id).map(obj -> mapper.toResponse(obj)));
     }
 
     @Override
